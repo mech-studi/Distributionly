eb888e0e2ed06b2a43d0022589de44727864ff7e
mech-studi <mc.studifiziere@gmail.com>
2020-05-08 17:00:01 +0000

    Fucking vs code insert errors

--- contracts/DomainKeeper.sol
+++ contracts/DomainKeeper.sol
@@ -196,7 +196,7 @@ contract DomainKeeper {
             auctions[dh].ended,
             auctions[dh].exists
         );
-    f
+    }
 
     function getAuctionStateBidder(string memory _domain) public view returns (address) {
         return (auctions[hashDomain(_domain)].highestBidder);
@@ -213,20 +213,5 @@ contract DomainKeeper {
     function getAuctionStateReturns(string memory _domain) public view returns (uint256) {
         return (auctions[hashDomain(_domain)].pendingReturns[msg.sender]);
     }
-unction getAuctionStateBidder(string memory _domain) public view returns (address) {
-        return (auctions[hashDomain(_domain)].highestBidder);
-    }
 
-    function getAuctionStateBid(string memory _domain) public view returns (uint256) {
-        return (auctions[hashDomain(_domain)].highestBid);
-    }
-
-    function getAuctionStateExists(string memory _domain) public view returns (bool) {
-        return (auctions[hashDomain(_domain)].exists);
-    }
-
-    function getAuctionStateReturns(string memory _domain) public view returns (uint256) {
-        return (auctions[hashDomain(_domain)].pendingReturns[msg.sender]);
-    }   return (auctions[hashDomain(_domain)].pendingReturns[msg.sender]);
-    }
 }
